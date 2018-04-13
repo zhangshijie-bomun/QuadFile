b7f855131e5077aeaafcfc133c9f5dcc35def7ca
Volkor The Barbarian Warrior <volkor@thecum.zone>
2018-04-13 16:08:15 +1000

    update FAQ, make slightly more SFW

---
+++ static/bg.js
@@ -0,0 +1,12 @@
+function bgraven (){
+    var body = document.getElementsByTagName('body')[0];
+    body.style.backgroundImage = 'url("/static/bg/raven.png")';
+}
+function bgjinx (){
+    var body = document.getElementsByTagName('body')[0];
+    body.style.backgroundImage = 'url("/static/bg/jinx.png")';
+}
+function bgblank (){
+    var body = document.getElementsByTagName('body')[0];
+    body.style.backgroundImage = 'url("")';
+}
